81e759140f077af4bfead26982ac1735b03f7f4b
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-04-20 17:02:44 +0600

    summing the natural numbers

--- natural.js
+++ natural.js
@@ -1,15 +1,35 @@
-/* return a function that returns all the natural numbers from 1 to n 
-sum of 1 to 5 which is : 15 */
+// /* return a function that returns all the natural numbers from 1 to n 
+// sum of 1 to 5 which is : 15 */
 
-function sumOfNaturalnumber(num){ 
+// function sumOfNaturalnumber(num){ 
+//   let sum = 0 ; 
+
+//   for (let i =1 ; i<=num; i++){ 
+// sum+=i; 
+
+// }
+// return sum; 
+
+// }
+
+// console.log(sumOfNaturalnumber(5)); 
+
+
+
+function sum_of_natural(num){ 
   let sum = 0 ; 
 
-  for (let i =1 ; i<=num; i++){ 
-sum+=i; 
+  for(let i =0; i<=num; i++){ 
+    sum+=i; 
+  }
+  return sum; 
 
 }
-return sum; 
 
-}
+console.log(sum_of_natural(6)); 
 
-console.log(sumOfNaturalnumber(5)); 
+
+
+
+
+
