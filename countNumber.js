000d72b19b15a5d0b00036f23009b413e204b501
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-04-24 23:15:55 +0600

    Counting the digits only, not adding them

--- countNumber.js
+++ countNumber.js
@@ -1,9 +1,17 @@
-function digit(num){
-let sum = 0; while(num>0){
-  sum++
-  num = Math.floor(num/10)
- }
-  return sum;
+function entschuldigung ( num )
+{
+      let count = 0; 
+
+    while (num>0){ 
+      count++; 
+      num = Math.floor(num/10);
+    }
+
+    return count;
 }
 
-console.log(digit(1094572345));
+console.log(entschuldigung(148350948));
+
+/* here everytime the loop runs and it counting how many times the loop is 
+being processing, then it's giving me a result. The use of math.floor only takes the 
+real actual numbers not the fractions one. This is how it's updating with an actual number not any fractions.  */
