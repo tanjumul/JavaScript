2335181c5eab948692829ed749f5abcf9f35b7a5
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-04-26 03:01:27 +0600

    solving fibonacci

---
+++ numberWhereareyou.js
@@ -0,0 +1,14 @@
+
+
+function harayGeche(num){ 
+
+let sum = 0; 
+for(let i  =0; i<num.length; i++){ 
+sum+=num[i];
+}
+return num.length*(num.length+1)/2 - sum ; 
+
+}
+
+
+console.log(harayGeche([3,0,1]));
