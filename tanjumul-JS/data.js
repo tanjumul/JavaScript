84f35b22078fa623035dced3fefa5cc8b40c5b7e
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-02 18:09:22 +0600

    null is a object

---
+++ tanjumul-JS/data.js
@@ -0,0 +1,27 @@
+"use strict " //This means , treat all code as a newer version of JavaScript 
+
+// alert('Tanjumul again !')  // we are using Nodejs not browser you moreO.. 
+
+//code readability should be there ! 
+//ecma script is there for latest standards tc39 you know ! 
+
+/** number , bigint , string , boolean , null 
+ * null is a standalone value  which is also prefered as a object
+ * boolean = true / false ; 
+ * symbol = unique
+ */
+
+/**
+ * non premitive ; 
+ * objects 
+ * 
+ */
+
+let newmachine = { 
+  key : 'tanjumul' 
+}
+
+console.log(newmachine.key); 
+
+console.log(typeof null ); // see null is a object 
+console.log(typeof undefined ); 
