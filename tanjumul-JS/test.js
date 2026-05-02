2a5f5135a0924e992eff66384d4eae9017b0bf00
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-02 17:47:40 +0600

    don't use var

--- tanjumul-JS/test.js
+++ tanjumul-JS/test.js
@@ -1,1 +1,13 @@
-console.log('ThisIsNode');
+const accountID = 1234445; 
+let accountEmail = 'tanjumul@gmail.com'; 
+var accountPassword = '1234'; 
+accountCity = 'Dhaka';
+
+accountEmail = 'tal@hc.com';
+accountPassword = 'Dud3P3rfect'; 
+accountCity = 'Chittagong'; 
+
+console.table([accountID, accountEmail , accountPassword]); 
+
+
+/**suggestion is , don't use the var because it mistmatches the scope declarations and so on ! */
