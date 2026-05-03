1e20a46e702c7a87d5e354bd1e557bad7288e162
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-03 15:26:32 +0600

    boolean conversins

--- tanjumul-JS/numberInvestigation.js
+++ tanjumul-JS/numberInvestigation.js
@@ -8,3 +8,24 @@ let ValidNumber = Number(score);
 console.log(typeof (ValidNumber)); 
 console.log(ValidNumber); 
 
+/** when it's a '101' it will convert easily but when it is 
+ * '101Tanjumul' mixed string + number then it turns to be very unusual in conversion 
+ * '101Tanjumul' => NaN 
+ * true => 1 ; 
+ * false => 0; 
+ */
+
+
+
+isLoggedIn = 1 ; 
+
+let boolisLoggedIn = Boolean(isLoggedIn); 
+console.log(boolisLoggedIn); 
+
+let isBoolean2 = false; 
+
+let boolisBoolean2 =Boolean(isBoolean2); 
+console.log(boolisBoolean2); 
+
+
+//when you use the 1 and convert it in the boolean then it gives like true , when youuse a string it will gives you a true , when you send it a null/0 / empty string it will send false , or 0 
