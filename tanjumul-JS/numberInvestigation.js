dc56f583171c26e7f53bcb7c665a051963e8fce2
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-03 15:18:22 +0600

    from a string to a number , check through the typeof it will show that a numer but in printing it will show a NaN. Be careful

---
+++ tanjumul-JS/numberInvestigation.js
@@ -0,0 +1,9 @@
+
+let score = "123acd"; 
+
+console.log(typeof score); 
+console.log(typeof (score)); 
+
+let ValidNumber = Number(score); 
+console.log(typeof (ValidNumber)); 
+console.log(ValidNumber); 
