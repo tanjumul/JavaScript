9d14739f9b531a157c8c2d844b591cdfe3a7fcea
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-03 16:15:26 +0600

    incremental flow and modes pre and post docs

---
+++ tanjumul-JS/operations.js
@@ -0,0 +1,19 @@
+let value = 3; 
+let neg = -3;
+let vall = -value; 
+
+console.log(vall); 
+
+let gamecounter = 100;
+
+++gamecounter; 
+console.log(gamecounter); 
+
+
+let x = 3 ; 
+const y = x++; 
+console.log(`x : ${x} & y : ${y}`);
+
+let a = 3 ; 
+const b = a++; 
+console.log(`a : ${a} & b ${b}`);
