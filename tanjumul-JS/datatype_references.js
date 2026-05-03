b6d8a96e7871448967b69807cd89adf172b95b75
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-03 17:02:55 +0600

    Symbol throws unique always

---
+++ tanjumul-JS/datatype_references.js
@@ -0,0 +1,41 @@
+//primitive dtypes 
+
+/** 7 segments like wise if you want to say like : tanjumul listen : 
+ * 
+ * 1. string ,
+ * 2.Number , 
+ * 3.booleans, 
+ * 4.null , 
+ * 5.undefined , 
+ * 6.symbol, 
+ * 7.Bigint.
+ * 
+ * 
+ */
+
+let score = 1100; 
+const scoreValue = 100.22; 
+
+const isLoggedIn = false ; 
+const outSideTemp = null ;
+
+let userEmail; 
+
+console.log(typeof outSideTemp); // non-primitive ? 
+
+
+const id = Symbol('134'); 
+const another_id = Symbol('134'); 
+
+console.log(id === another_id); 
+
+
+
+//Non-primitive Dtypes
+
+
+/**
+ * 1. Objects 
+ * 2.Functions , 
+ * 3. Array
+ */
