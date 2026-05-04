310c4dd20dd24e2d89c9f69eec606f74eaccccdb
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-04 18:25:16 +0600

    on keydown and event.key && element.value

---
+++ dynamic_practice/scripts/11-ToDo-list.js
@@ -0,0 +1,10 @@
+
+const todolist = []; 
+
+function addTodo (){ 
+  const inputElement =  document.querySelector('.js-input-name'); 
+
+  const name = inputElement.value ; 
+  console.log(name); 
+
+}
