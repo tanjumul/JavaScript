e8836f0886ce18e2047a51595aec387b9398608a
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-05 02:13:25 +0600

    tracking : DSA - 1

--- tanjumul-JS/DSA/dsa-1.js
+++ tanjumul-JS/DSA/dsa-1.js
@@ -1,15 +1,26 @@
 
-// const num = prompt('Enter your number for the loop ! ');  
+// // const num = prompt('Enter your number for the loop ! ');  
 
-function calculate ()
-{ 
-const takeNumber = document.querySelector('.givein'); 
-const Intlen = takeNumber.value; 
+// function calculate ()
+// { 
+// const takeNumber = document.querySelector('.givein'); 
+// const Intlen = takeNumber.value; 
 
-  for(let i=1; i<=Intlen; i++){ 
+//   for(let i=1; i<=Intlen; i++){ 
 
-  let contains = Intlen - i 
-  console.log(contains);
-}
-console.log(takeNumber); 
+//   let contains = Intlen - i+1; 
+//   console.log(contains);
+// }
+// console.log(takeNumber); 
+// }
+
+
+const num = 10; 
+
+for(let i =1; i<= num ; i++){ 
+  if(i%2 == 0 ){ 
+    console.log(` even num : ${i}`); 
+  } else{ 
+    console.log(`this is the odd num : ${i}`); 
+  }
 }
