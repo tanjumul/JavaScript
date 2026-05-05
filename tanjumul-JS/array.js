336c3fcbe2aefbe4b3588051b96884fb3f4bf450
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-05 15:01:24 +0600

    using : .unshift() to input value at the beginning of the myArr

---
+++ tanjumul-JS/array.js
@@ -0,0 +1,27 @@
+
+
+const myArr = [0,1,2,3,4,5,6]
+const myHeros = ["Superman", "Batman"]; 
+
+const myArr2 = new Array(1,3,4,2); 
+
+
+console.log(myArr[1]);
+console.log(myArr2[1]); 
+
+
+console.log(myHeros[0]);
+
+//Array Methods 
+
+
+myArr.push(7); 
+
+myArr.pop(7); 
+
+console.log(myArr); 
+
+
+
+myArr.unshift(9); 
+console.log(myArr); 
