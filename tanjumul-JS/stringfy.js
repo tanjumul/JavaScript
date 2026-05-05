4813eac6a85255fc3c403ba329579b06718964a5
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-05 12:52:48 +0600

    string methods and objects that i can use

--- tanjumul-JS/stringfy.js
+++ tanjumul-JS/stringfy.js
@@ -9,3 +9,26 @@ console.log(myGame[2]);
 
 console.log(myGame.__proto__); //it throws an object on the console
 
+/**character in a string at , and the index of a string and the character on it  */
+
+
+console.log(myGame.charAt(5));
+console.log(myGame.indexOf('i')); 
+
+
+const newString = '   tanjumul    '; 
+console.log(newString); 
+console.log(newString.trim()); 
+
+const url = "https://Tanjumul.con/tanjumul%20alom/"
+
+console.log(url.replace('%20', '_'));
+
+console.log(url.includes('matt hardy')); 
+const ichBin = "Ich labbe ,  Deutschland. Woher, kommst aus du ? Guten, morgan ! "; 
+
+
+const myName = ichBin.split(','); 
+
+console.log(myName); 
+console.log(myName[2]); 
