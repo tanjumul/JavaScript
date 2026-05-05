6178a1661507f729335d470c8323112848ff47d8
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-06 00:53:16 +0600

    storing the array elements into a variable using loop

--- dynamic_practice/scripts/11-ToDo-list.js
+++ dynamic_practice/scripts/11-ToDo-list.js
@@ -1,11 +1,14 @@
 
 const todolist = ['make dinner ', 'wash dishes']; 
 
+let todoListHtml = ' '; 
+
 for(let i = 0 ; i< todolist.length; i++){
 const todo = todolist[i];
 const html = `<p> ${todo}</p>`;
+todoListHtml += html; 
 }
-
+console.log(todoListHtml); 
 function addTodo (){ 
   const inputElement =  document.querySelector('.js-input-name'); 
 
