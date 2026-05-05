b2d021b2f019fc38b97b9064c32bf12004e16ccd
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-05 12:34:27 +0600

    thorows an object on the console

---
+++ tanjumul-JS/stringfy.js
@@ -0,0 +1,11 @@
+
+repos = 50; 
+
+console.log(`My github repo count is ${repos}`);
+
+const myGame = new String('Rainbow 6 siege'); 
+
+console.log(myGame[2]); 
+
+console.log(myGame.__proto__); //it throws an object on the console
+
