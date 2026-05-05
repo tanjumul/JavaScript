8a2dc3ff06763e5d2f33e71db6f66daa22816a9c
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-05 16:06:39 +0600

    Important file for Array isArray , from() , flat(infinity) , sprade array[...] , concat

--- tanjumul-JS/array2.js
+++ tanjumul-JS/array2.js
@@ -12,3 +12,37 @@ const musicIlove = [ 'limBiskitz' , 'Linkinpark' , 'Drake' , 'TravisScott' ,'Ale
 const allNewfavs = [...musicIlove , ... DC_superHeroes]; 
 
 console.log(allNewfavs);
+
+
+
+const another_situation = [1,2,3,4,5,[6,7,8,9],10,[11,12,[13,14]]]; 
+
+const real_another_array = another_situation.flat(Infinity); //if you want you can declare the depth but here i don't want to take any hicaps that's why i declared infinity. 
+
+
+console.log(real_another_array); 
+
+/** interesting part is , here it's showin all the array
+ *  elements no matter where it is , no matter what it is. */
+
+
+const dine = 'tanju'
+console.log(Array.isArray(dine)); //questioning the element
+
+console.log(Array.from(dine)); //it can seperate all the character into an array. 
+
+console.log(Array.from({name : 'Tanjumul'})); 
+// it gives you an empty string but you should've added a target if it is a key or value ? 
+
+
+ const score1 = 100; 
+ const score2 = 200; 
+ const score3 = 300; 
+ const score4 = 400; 
+
+ console.log(Array.of(score1,score2,score3,score4)); 
+ 
+
+
+
+
