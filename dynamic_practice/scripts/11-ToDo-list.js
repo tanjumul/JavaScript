2fc4bed76bf47f9a025c0ff9f231317e8dafe4ed
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-06 00:50:21 +0600

    showing the array element on the html page

--- dynamic_practice/scripts/11-ToDo-list.js
+++ dynamic_practice/scripts/11-ToDo-list.js
@@ -1,16 +1,22 @@
 
-const todolist = []; 
+const todolist = ['make dinner ', 'wash dishes']; 
+
+for(let i = 0 ; i< todolist.length; i++){
+const todo = todolist[i];
+const html = `<p> ${todo}</p>`;
+}
 
 function addTodo (){ 
   const inputElement =  document.querySelector('.js-input-name'); 
 
   const name = inputElement.value ; 
+  inputElement.value = ' '; 
   console.log(name); 
 
   todolist.push(name); 
   console.log(todolist); 
 
-  inputElement.value = ' '; 
+  
 
   const para = document.querySelector('.writes'); 
   para.innerHTML = `${name}`;
