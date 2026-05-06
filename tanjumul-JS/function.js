31d783d6c39dcc207679a563266876b97fd83449
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-06 17:10:39 +0600

    function declaration and the calling of the function

---
+++ tanjumul-JS/function.js
@@ -0,0 +1,23 @@
+/** 
+ * what is argument and parameters 
+ * 
+ * functon Tanju(playing , gmaes )
+ * here - playing and games are parameters 
+ * 
+ * 
+ * after finishing the funciton when i will call that i will pass an argument of values there , 
+ * 
+ * 
+ *tanju(CounterStrike_2 , FPS); 
+
+ the counterstrike_2 and fps here is the argument
+ */
+
+ function addTwoNumbs (number1, number2){ 
+
+  return number1 + number2; 
+
+ }
+
+ console.log(addTwoNumbs(23,22)); 
+
