1e5935d5bdde46aa3bc84f05ed7fcf133580fd1e
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-06 12:11:58 +0600

    objects and symbol

--- tanjumul-JS/objectss.js
+++ tanjumul-JS/objectss.js
@@ -1,6 +1,8 @@
 //singleton : 1 object 
 //object literals 
 
+
+const mySymble = Symbol('key1');
 const jsUser ={ 
   name : 'Tanjumul', 
   age: 26, 
@@ -8,10 +10,29 @@ const jsUser ={
   location : 'Gaibandha',
   email : 'tanjumul@aylo.com', 
   isLoggedIn : false , 
-  lastLoginDays : ["Monday", "friday"]  
+  lastLoginDays : ["Monday", "friday"] , 
+  [mySymble] : 'mykey'
 
 }
 
 console.log(jsUser.age); 
 console.log(jsUser['email']); 
 console.log(jsUser['full name']); 
+console.log(jsUser[mySymble]);
+console.log(typeof jsUser[mySymble]);
+
+jsUser.email = 'Tanjumul.gpt@gmail.com';
+// Object.freeze(jsUser); 
+
+jsUser.email= 'tanjumul@microsoft.com';
+
+console.log(jsUser); 
+
+
+jsUser.greetings = function(){ 
+  console.log('Hello , Js user ! '); 
+
+}
+
+console.log(jsUser.greetings());
+
