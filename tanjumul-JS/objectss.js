2d0193d76ed6369c3ee89253852771de619cb5e7
2019331101-Tanjumul <crackerroot4@gmail.com>
2026-05-06 11:14:34 +0600

    declaring the key pair in different way

---
+++ tanjumul-JS/objectss.js
@@ -0,0 +1,17 @@
+//singleton : 1 object 
+//object literals 
+
+const jsUser ={ 
+  name : 'Tanjumul', 
+  age: 26, 
+  'full name' : 'Tanjumul Alom',
+  location : 'Gaibandha',
+  email : 'tanjumul@aylo.com', 
+  isLoggedIn : false , 
+  lastLoginDays : ["Monday", "friday"]  
+
+}
+
+console.log(jsUser.age); 
+console.log(jsUser['email']); 
+console.log(jsUser['full name']); 
